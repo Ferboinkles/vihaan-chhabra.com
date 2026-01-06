35296eb97f14661724c21e2fa5c3cd59c3d16418
ferboinkles <ferboinkles@gmail.com>
2026-01-06 17:57:07 +0000

    Made a good start with adding the layout.css getting ideas, making a title and just bug fixing!

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,19 @@
 <script>
-	import "../app.css";
-	import favicon from "$lib/assets/favicon.svg";
+	import './layout.css';
 
 	let { children } = $props();
+
 </script>
 
-<svelte:head>
-	<link rel="icon" href={favicon} />
-</svelte:head>
+<div class="national-park">{@render children?.()}</div>
 
-{@render children?.()}
+<style>
+	@import url("https://fonts.googleapis.com/css2?family=National+Park:wght@200..800&display=swap");
+
+	.national-park {
+		font-family: "National Park", sans-serif;
+		font-optical-sizing: auto;
+		font-weight: 600;
+		font-style: normal;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Meow</p>
+<div>
+<h1 class="text-center text-4xl text-white">Welcome to vihaan-chhabra.com!  </h1>
+<p class="text-center text-3xl  text-white" >This is my personal website and I am a student who loves coding! </p>
+<p class="text-center text-3xl text-white ">Check out my projects below</p>
+
+	<button class=""></button>
+</div>
