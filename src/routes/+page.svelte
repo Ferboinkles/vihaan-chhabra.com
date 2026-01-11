b07f982dcf8c976bc9de264eb8d59c644d39992a
ferboinkles <ferboinkles@gmail.com>
2026-01-11 17:36:03 +0000

    OK i have now scrapped everything and am trying to get the most annoying UI ever

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,131 @@
 <div>
 <h1 class="text-center text-4xl text-white">Welcome to vihaan-chhabra.com!  </h1>
-<p class="text-center text-3xl  text-white" >This is my personal website and I am a student who loves coding! </p>
-<p class="text-center text-3xl text-white ">Check out my projects below</p>
-
-	<button class=""></button>
+<p class="text-center text-3xl text-white">I am going to make this website as horrible to use as possible!</p>
+	<p class="text-center text-3xl text-white">Good luck finding the button to go to the actual website!</p>
+	<button class="text-4xl inline-block max-w-fit whitespace-normal rounded-[0.5em] bg-linear-to-r from-[#035fff] to-[#03fbff] p-[0.5em] my-1 hover:scale-710 animate-spin">
+	This one!</button>
+	<button class="text-4xl inline-block max-w-fit whitespace-normal rounded-[0.5em] bg-linear-to-r from-[#035fff] to-[#03fbff] p-[0.5em] my-1 hover:scale-310 animate-spin">
+		This one!</button>
+	<button class="text-4xl inline-block max-w-fit whitespace-normal rounded-[0.5em] bg-linear-to-r from-[#035fff] to-[#03fbff] p-[0.5em] my-1 hover:scale-310 animate-spin">
+		This one!</button><button class="text-4xl inline-block max-w-fit whitespace-normal rounded-[0.5em] bg-linear-to-r from-[#035fff] to-[#03fbff] p-[0.5em] my-1 hover:scale-310 animate-spin">
+	This one!</button>
+	<button class="text-4xl inline-block max-w-fit whitespace-normal rounded-[0.5em] bg-linear-to-r from-[#035fff] to-[#03fbff] p-[0.5em] my-1 hover:scale-310 animate-spin">
+	This one!</button>
+	<button class="text-4xl inline-block max-w-fit whitespace-normal rounded-[0.5em] bg-linear-to-r from-[#035fff] to-[#03fbff] p-[0.5em] my-1 hover:scale-310 animate-spin">
+	This one!</button>
+	<button class="text-4xl inline-block max-w-fit whitespace-normal rounded-[0.5em] bg-linear-to-r from-[#035fff] to-[#03fbff] p-[0.5em] my-1 hover:scale-310 animate-spin">
+	This one!</button>
+	<button class="text-4xl inline-block max-w-fit whitespace-normal rounded-[0.5em] bg-linear-to-r from-[#035fff] to-[#03fbff] p-[0.5em] my-1 hover:scale-310 animate-spin">
+	This one!</button>
+	<button class="text-4xl inline-block max-w-fit whitespace-normal rounded-[0.5em] bg-linear-to-r from-[#035fff] to-[#03fbff] p-[0.5em] my-1 hover:scale-310 animate-spin">
+	This one!</button>
+	<button class="text-4xl inline-block max-w-fit whitespace-normal rounded-[0.5em] bg-linear-to-r from-[#035fff] to-[#03fbff] p-[0.5em] my-1 hover:scale-310 animate-spin">
+	This one!</button>
+	<button class="text-4xl inline-block max-w-fit whitespace-normal rounded-[0.5em] bg-linear-to-r from-[#035fff] to-[#03fbff] p-[0.5em] my-1 hover:scale-310 animate-spin">
+	This one!</button>
+	<button class="text-4xl inline-block max-w-fit whitespace-normal rounded-[0.5em] bg-linear-to-r from-[#035fff] to-[#03fbff] p-[0.5em] my-1 hover:scale-310 animate-spin">
+	This one!</button>
+	<button class="text-4xl inline-block max-w-fit whitespace-normal rounded-[0.5em] bg-linear-to-r from-[#035fff] to-[#03fbff] p-[0.5em] my-1 hover:scale-310 animate-spin">
+	This one!</button>
+	<button class="text-4xl inline-block max-w-fit whitespace-normal rounded-[0.5em] bg-linear-to-r from-[#035fff] to-[#03fbff] p-[0.5em] my-1 hover:scale-310 animate-spin">
+	This one!</button>
+	<button class="text-4xl inline-block max-w-fit whitespace-normal rounded-[0.5em] bg-linear-to-r from-[#035fff] to-[#03fbff] p-[0.5em] my-1 hover:scale-310 animate-spin">
+	This one!</button>
+	<button class="text-4xl inline-block max-w-fit whitespace-normal rounded-[0.5em] bg-linear-to-r from-[#035fff] to-[#03fbff] p-[0.5em] my-1 hover:scale-310 animate-spin">
+	This one!</button>
+	<button class="text-4xl inline-block max-w-fit whitespace-normal rounded-[0.5em] bg-linear-to-r from-[#035fff] to-[#03fbff] p-[0.5em] my-1 hover:scale-310 animate-spin">
+	This one!</button>
+	<button class="text-4xl inline-block max-w-fit whitespace-normal rounded-[0.5em] bg-linear-to-r from-[#035fff] to-[#03fbff] p-[0.5em] my-1 hover:scale-310 animate-spin">
+	This one!</button>
+	<button class="text-4xl inline-block max-w-fit whitespace-normal rounded-[0.5em] bg-linear-to-r from-[#035fff] to-[#03fbff] p-[0.5em] my-1 hover:scale-310 animate-spin">
+	This one!</button>
+	<button class="text-4xl inline-block max-w-fit whitespace-normal rounded-[0.5em] bg-linear-to-r from-[#035fff] to-[#03fbff] p-[0.5em] my-1 hover:scale-310 animate-spin">
+	This one!</button>
+	<button class="text-4xl inline-block max-w-fit whitespace-normal rounded-[0.5em] bg-linear-to-r from-[#035fff] to-[#03fbff] p-[0.5em] my-1 hover:scale-310 animate-spin">
+	This one!</button>
+	<button class="text-4xl inline-block max-w-fit whitespace-normal rounded-[0.5em] bg-linear-to-r from-[#035fff] to-[#03fbff] p-[0.5em] my-1 hover:scale-310 animate-spin">
+	This one!</button>
+	<button class="text-4xl inline-block max-w-fit whitespace-normal rounded-[0.5em] bg-linear-to-r from-[#035fff] to-[#03fbff] p-[0.5em] my-1 hover:scale-310 animate-spin">
+	This one!</button>
+	<button class="text-4xl inline-block max-w-fit whitespace-normal rounded-[0.5em] bg-linear-to-r from-[#035fff] to-[#03fbff] p-[0.5em] my-1 hover:scale-310 animate-spin">
+	This one!</button>
+	<button class="text-4xl inline-block max-w-fit whitespace-normal rounded-[0.5em] bg-linear-to-r from-[#035fff] to-[#03fbff] p-[0.5em] my-1 hover:scale-310 animate-spin">
+	This one!</button>
+	<button class="text-4xl inline-block max-w-fit whitespace-normal rounded-[0.5em] bg-linear-to-r from-[#035fff] to-[#03fbff] p-[0.5em] my-1 hover:scale-310 animate-spin">
+	This one!</button>
+	<button class="text-4xl inline-block max-w-fit whitespace-normal rounded-[0.5em] bg-linear-to-r from-[#035fff] to-[#03fbff] p-[0.5em] my-1 hover:scale-310 animate-spin">
+	This one!</button>
+	<button class="text-4xl inline-block max-w-fit whitespace-normal rounded-[0.5em] bg-linear-to-r from-[#035fff] to-[#03fbff] p-[0.5em] my-1 hover:scale-310 animate-spin">
+	This one!</button>
+	<button class="text-4xl inline-block max-w-fit whitespace-normal rounded-[0.5em] bg-linear-to-r from-[#035fff] to-[#03fbff] p-[0.5em] my-1 hover:scale-310 animate-spin">
+	This one!</button>
+	<button class="text-4xl inline-block max-w-fit whitespace-normal rounded-[0.5em] bg-linear-to-r from-[#035fff] to-[#03fbff] p-[0.5em] my-1 hover:scale-310 animate-spin">
+	This one!</button>
+	<button class="text-4xl inline-block max-w-fit whitespace-normal rounded-[0.5em] bg-linear-to-r from-[#035fff] to-[#03fbff] p-[0.5em] my-1 hover:scale-310 animate-spin">
+	This one!</button><button class="text-4xl inline-block max-w-fit whitespace-normal rounded-[0.5em] bg-linear-to-r from-[#035fff] to-[#03fbff] p-[0.5em] my-1 hover:scale-310 animate-spin">
+	This one!</button>
+	<button class="text-4xl inline-block max-w-fit whitespace-normal rounded-[0.5em] bg-linear-to-r from-[#035fff] to-[#03fbff] p-[0.5em] my-1 hover:scale-310 animate-spin">
+	This one!</button>
+	<button class="text-4xl inline-block max-w-fit whitespace-normal rounded-[0.5em] bg-linear-to-r from-[#035fff] to-[#03fbff] p-[0.5em] my-1 hover:scale-310 animate-spin">
+	This one!</button>
+	<button class="text-4xl inline-block max-w-fit whitespace-normal rounded-[0.5em] bg-linear-to-r from-[#035fff] to-[#03fbff] p-[0.5em] my-1 hover:scale-310 animate-spin">
+	This one!</button>
+	<button class="text-4xl inline-block max-w-fit whitespace-normal rounded-[0.5em] bg-linear-to-r from-[#035fff] to-[#03fbff] p-[0.5em] my-1 hover:scale-310 animate-spin">
+	This one!</button>
+	<button class="text-4xl inline-block max-w-fit whitespace-normal rounded-[0.5em] bg-linear-to-r from-[#035fff] to-[#03fbff] p-[0.5em] my-1 hover:scale-310 animate-spin">
+	This one!</button>
+	<button class="text-4xl inline-block max-w-fit whitespace-normal rounded-[0.5em] bg-linear-to-r from-[#035fff] to-[#03fbff] p-[0.5em] my-1 hover:scale-310 animate-spin">
+	This one!</button>
+	<button class="text-4xl inline-block max-w-fit whitespace-normal rounded-[0.5em] bg-linear-to-r from-[#035fff] to-[#03fbff] p-[0.5em] my-1 hover:scale-310 animate-spin">
+	This one!</button>
+	<button class="text-4xl inline-block max-w-fit whitespace-normal rounded-[0.5em] bg-linear-to-r from-[#035fff] to-[#03fbff] p-[0.5em] my-1 hover:scale-310 animate-spin">
+	This one!</button>
+	<button class="text-4xl inline-block max-w-fit whitespace-normal rounded-[0.5em] bg-linear-to-r from-[#035fff] to-[#03fbff] p-[0.5em] my-1 hover:scale-310 animate-spin">
+	This one!</button>
+	<button class="text-4xl inline-block max-w-fit whitespace-normal rounded-[0.5em] bg-linear-to-r from-[#035fff] to-[#03fbff] p-[0.5em] my-1 hover:scale-310 animate-spin">
+	This one!</button>
+	<button class="text-4xl inline-block max-w-fit whitespace-normal rounded-[0.5em] bg-linear-to-r from-[#035fff] to-[#03fbff] p-[0.5em] my-1 hover:scale-310 animate-spin">
+	This one!</button>
+	<button class="text-4xl inline-block max-w-fit whitespace-normal rounded-[0.5em] bg-linear-to-r from-[#035fff] to-[#03fbff] p-[0.5em] my-1 hover:scale-310 animate-spin">
+	This one!</button>
+	<button class="text-4xl inline-block max-w-fit whitespace-normal rounded-[0.5em] bg-linear-to-r from-[#035fff] to-[#03fbff] p-[0.5em] my-1 hover:scale-310 animate-spin">
+	This one!</button>
+	<button class="text-4xl inline-block max-w-fit whitespace-normal rounded-[0.5em] bg-linear-to-r from-[#035fff] to-[#03fbff] p-[0.5em] my-1 hover:scale-310 animate-spin">
+	This one!</button>
+	<button class="text-4xl inline-block max-w-fit whitespace-normal rounded-[0.5em] bg-linear-to-r from-[#035fff] to-[#03fbff] p-[0.5em] my-1 hover:scale-310 animate-spin">
+	This one!</button>
+	<button class="text-4xl inline-block max-w-fit whitespace-normal rounded-[0.5em] bg-linear-to-r from-[#035fff] to-[#03fbff] p-[0.5em] my-1 hover:scale-310 animate-spin">
+	This one!</button>
+	<button class="text-4xl inline-block max-w-fit whitespace-normal rounded-[0.5em] bg-linear-to-r from-[#035fff] to-[#03fbff] p-[0.5em] my-1 hover:scale-310 animate-spin">
+	This one!</button>
+	<button class="text-4xl inline-block max-w-fit whitespace-normal rounded-[0.5em] bg-linear-to-r from-[#035fff] to-[#03fbff] p-[0.5em] my-1 hover:scale-310 animate-spin">
+	This one!</button>
+	<button class="text-4xl inline-block max-w-fit whitespace-normal rounded-[0.5em] bg-linear-to-r from-[#035fff] to-[#03fbff] p-[0.5em] my-1 hover:scale-310 animate-spin">
+	This one!</button>
+	<button class="text-4xl inline-block max-w-fit whitespace-normal rounded-[0.5em] bg-linear-to-r from-[#035fff] to-[#03fbff] p-[0.5em] my-1 hover:scale-310 animate-spin">
+	This one!</button>
+	<button class="text-4xl inline-block max-w-fit whitespace-normal rounded-[0.5em] bg-linear-to-r from-[#035fff] to-[#03fbff] p-[0.5em] my-1 hover:scale-310 animate-spin">
+	This one!</button>
+	<button class="text-4xl inline-block max-w-fit whitespace-normal rounded-[0.5em] bg-linear-to-r from-[#035fff] to-[#03fbff] p-[0.5em] my-1 hover:scale-310 animate-spin">
+	This one!</button>
+	<button class="text-4xl inline-block max-w-fit whitespace-normal rounded-[0.5em] bg-linear-to-r from-[#035fff] to-[#03fbff] p-[0.5em] my-1 hover:scale-310 animate-spin">
+	This one!</button>
+	<button class="text-4xl inline-block max-w-fit whitespace-normal rounded-[0.5em] bg-linear-to-r from-[#035fff] to-[#03fbff] p-[0.5em] my-1 hover:scale-310 animate-spin">
+	This one!</button>
+	<button class="text-4xl inline-block max-w-fit whitespace-normal rounded-[0.5em] bg-linear-to-r from-[#035fff] to-[#03fbff] p-[0.5em] my-1 hover:scale-310 animate-spin">
+	This one!</button>
+	<button class="text-4xl inline-block max-w-fit whitespace-normal rounded-[0.5em] bg-linear-to-r from-[#035fff] to-[#03fbff] p-[0.5em] my-1 hover:scale-310 animate-spin">
+	This one!</button>
+	<button class="text-4xl inline-block max-w-fit whitespace-normal rounded-[0.5em] bg-linear-to-r from-[#035fff] to-[#03fbff] p-[0.5em] my-1 hover:scale-310 animate-spin">
+	This one!</button>
+	<button class="text-4xl inline-block max-w-fit whitespace-normal rounded-[0.5em] bg-linear-to-r from-[#035fff] to-[#03fbff] p-[0.5em] my-1 hover:scale-310 animate-spin">
+	This one!</button>
+	<button class="text-4xl inline-block max-w-fit whitespace-normal rounded-[0.5em] bg-linear-to-r from-[#035fff] to-[#03fbff] p-[0.5em] my-1 hover:scale-310 animate-spin">
+	This one!</button>
+	<button class="text-4xl inline-block max-w-fit whitespace-normal rounded-[0.5em] bg-linear-to-r from-[#035fff] to-[#03fbff] p-[0.5em] my-1 hover:scale-310 animate-spin">
+	This one!</button>
+	<button class="text-4xl inline-block max-w-fit whitespace-normal rounded-[0.5em] bg-linear-to-r from-[#035fff] to-[#03fbff] p-[0.5em] my-1 hover:scale-310 animate-spin">
+	This one!</button>
+	<button class="text-4xl inline-block max-w-fit whitespace-normal rounded-[0.5em] bg-linear-to-r from-[#035fff] to-[#03fbff] p-[0.5em] my-1 hover:scale-310 animate-spin">
+	This one!</button>
 </div>
